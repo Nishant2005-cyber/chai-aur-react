bd57542b8f5796c80df8d149bd6eadddb20c7449
Nishant2005-cyber <nishukhandelwal012@gmail.com>
2026-08-19 23:03:17 +0530

    fixed some errors

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,5 +1,6 @@
 import conf from '../conf/conf.js';
-import { client, Account, ID} from "appwrite";
+import { Client, Account, ID } from "appwrite";
+
 
 export class AuthService {
     client = new Client();
@@ -10,15 +11,17 @@ export class AuthService {
             .setEndpoint(conf.appwriteUrl)
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
+            
     }
 
     async createAccount({email, password, name}) {
         try {
             const userAccount = await this.account.create(ID.unique(), email, password, name);
-            if(userAccount) {
+            if (userAccount) {
+                // call another method
                 return this.login({email, password});
             } else {
-                return userAccount;
+               return  userAccount;
             }
         } catch (error) {
             throw error;
@@ -37,20 +40,23 @@ export class AuthService {
         try {
             return await this.account.get();
         } catch (error) {
-            console.log("Appwrite service :: getCurrentUser :: error", error);
+            console.log("Appwrite serive :: getCurrentUser :: error", error);
         }
+
         return null;
     }
 
     async logout() {
+
         try {
             await this.account.deleteSessions();
         } catch (error) {
-            console.log("Appwrite service :: logout :: error", error);
+            console.log("Appwrite serive :: logout :: error", error);
         }
     }
 }
 
 const authService = new AuthService();
 
-export default AuthService
+export default authService
+
